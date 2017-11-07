4bbc27e3aeb2bebe3faa4c79bcda8bf51a90bc71
Angelo Cigna <acigna1982@gmail.com>
2017-11-07 01:01:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ randomNumber = Math.random() * 20 + 0;
 
 randomInteger = Math.floor(randomNumber);
 
-randomUserID = Math.ceil(randomInteger) + newID
+randomUserID = Math.ceil(randomInteger) + 100000000
